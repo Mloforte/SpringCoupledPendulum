fd468f35c4be81c41505729bb5e323ec1b4c1512
Mateo-lab <67467656+Mateo-lab@users.noreply.github.com>
2024-05-23 00:19:50 +0200

    Fourrier transform plot and damping factor modified + functions that organized  plots

--- Code/Projet_V2.py
+++ Code/Projet_V2.py
@@ -6,16 +6,10 @@ from math import sin, cos, pi, sqrt
 import pygame
 import sys
 from scipy.fft import fft, fftfreq
-import sympy as sp  
+import sympy as sp
 
 
 width, height = 800, 400
-
-# # Initialisation of Pygame
-pygame.init()
-
-# # Creation of the fenêtre
-background = pygame.display.set_mode((800, 800))
 
 # COLORS
 white = (255, 255, 255)
@@ -30,14 +24,14 @@ Out = False  # if True,out of while loop, and close pygame
 m1 = 100  # pendulum mass 1
 m2 = 100  # pendulum mass 2
 l = 50  # suspension length
-d = 100  # distance between pendulums where the spring is attached
-k = 5.0  # coefficient of spring stiffness
+d = 40  # distance between pendulums where the spring is attached
+k = 50.0  # coefficient of spring stiffness
 g = 9.8  # acceleration due to gravity
-x = 0.3  # damping factor for lose of energy due to friction
+D = 0.9  # damping factor
 acceleration = False
 
 # Initial conditions
-theta1_0 = -pi/2  # deviation of the first pendulum
+theta1_0 = -pi/4  # deviation of the first pendulum
 theta2_0 = -pi/4  # deviation of the second pendulum
 dot_theta1_0 = 0.0  # intial angular velocity of the first pendulum
 dot_theta2_0 = 0.0  # initial angular velocity of the second pendulum
@@ -97,52 +91,168 @@ def G_adim(y, t):
     d_theta1 = dtheta1
 
     dd_theta1 = A*sin(theta1) - B*sin(theta1 - theta2) * \
-        (1 - (d / sqrt(d**2 + 2*l**2*(1 - cos(theta1 + theta2))))) - x*dtheta1
-    
+        (1 - (d / sqrt(d**2 + 2*l**2*(1 - cos(theta1 + theta2))))) - D*dtheta1
+
     d_theta2 = dtheta2
 
     dd_theta2 = A*sin(theta2) - C*sin(theta2 - theta1) * \
-        (1 - (d / sqrt(d**2 + 2*l**2*(1 - cos(theta1 + theta2))))) - x*dtheta2
-    
+        (1 - (d / sqrt(d**2 + 2*l**2*(1 - cos(theta1 + theta2))))) - D*dtheta2
+
     return np.array([d_theta1, dd_theta1, d_theta2, dd_theta2])
 
 
-# Définition des variables symboliques pour le calcul de la matrice jacobienne
-theta1_sym, dtheta1_sym, theta2_sym, dtheta2_sym = sp.symbols('theta1 dtheta1 theta2 dtheta2')
-y_sym = sp.Matrix([theta1_sym, dtheta1_sym, theta2_sym, dtheta2_sym])
+def Plotgraph(soltheta1, sold_theta1, soltheta2, sold_theta2):
+    plt.figure(figsize=(10, 40))
 
-# Définition des équations différentielles symboliques
-d_theta1_sym = dtheta1_sym
-dd_theta1_sym = A*sp.sin(theta1_sym) - B*sp.sin(theta1_sym - theta2_sym) * \
-    (1 - (d / sp.sqrt(d**2 + 2*l**2*(1 - sp.cos(theta1_sym + theta2_sym))))) - x*dtheta1_sym
-d_theta2_sym = dtheta2_sym
-dd_theta2_sym = A*sp.sin(theta2_sym) - C*sp.sin(theta2_sym - theta1_sym) * \
-    (1 - (d / sp.sqrt(d**2 + 2*l**2*(1 - sp.cos(theta1_sym + theta2_sym))))) - x*dtheta2_sym
+    plt.subplot(5, 1, 1)
+    plt.plot(soltheta1, sold_theta1, label='theta1')
+    plt.plot(soltheta2, sold_theta1, label='theta2')
+    plt.xlabel('dtheta1')
+    plt.ylabel('theta')
+    plt.legend()
+    plt.grid(True)
 
-# Définition du vecteur de dérivées
-f_sym = sp.Matrix([d_theta1_sym, dd_theta1_sym, d_theta2_sym, dd_theta2_sym])
+    plt.subplot(5, 1, 2)
+    plt.plot(soltheta1, sold_theta2, label='theta1')
+    plt.plot(soltheta2, sold_theta2, label='theta2')
+    plt.xlabel('dtheta2')
+    plt.ylabel('theta')
+    plt.legend()
+    plt.grid(True)
 
-# Calcul de la matrice jacobienne
-J = f_sym.jacobian(y_sym)
+    plt.subplot(5, 1, 3)
+    plt.plot(sold_theta1, soltheta1, label='dtheta1')
+    plt.plot(sold_theta2, soltheta1, label='dtheta2')
+    plt.xlabel('theta1')
+    plt.ylabel('dtheta')
+    plt.legend()
+    plt.grid(True)
 
-# Optionnellement, évaluation de la matrice jacobienne à des conditions initiales spécifiques
-initial_conditions = {
-    theta1_sym: theta1_0,
-    dtheta1_sym: dot_theta1_0,
-    theta2_sym: theta2_0,
-    dtheta2_sym: dot_theta2_0
-}
-J_evaluated = J.evalf(subs=initial_conditions)
-print("\nMatrice jacobienne évaluée aux conditions initiales:")
-sp.pprint(J_evaluated)
+    plt.subplot(5, 1, 4)
+    plt.plot(sold_theta1, soltheta2, label='dtheta1')
+    plt.plot(sold_theta2, soltheta2, label='dtheta2')
+    plt.xlabel('theta2')
+    plt.ylabel('dtheta')
+    plt.legend()
+    plt.grid(True)
 
+    plt.subplots_adjust(hspace=0.5)
+
+    plt.show()
+
+
+def PlotTheta(soltheta1, soltheta2):
+    plt.plot(t, soltheta1, label='theta1')
+    plt.plot(t, soltheta2, label='theta2')
+    plt.xlabel('Temps')
+    plt.ylabel('theta')
+    plt.title('theta1 et theta2 en fonction du temps')
+    plt.xlim(0, 100)
+    plt.grid(True)
+    plt.legend()
+    plt.show()
+
+
+def PlotFourier(theta1_data, theta2_data):
+    # Collect data
+    theta1_data = soltheta1  # Oscillation data for pendulum 1
+    theta2_data = soltheta2  # Oscillation data for pendulum 2
+
+    # Sampling frequency
+    fs = 1 / (t[1] - t[0])  # Sample rate, inverse of time step
+
+    # Perform FFT
+    theta1_fft = fft(theta1_data)
+    theta2_fft = fft(theta2_data)
+
+    # Frequency axis
+    freqs = fftfreq(len(theta1_data), 1 / fs)
+
+    # Plot FFT results
+    plt.figure()
+    plt.stem(freqs, np.abs(theta1_fft), linefmt='C0-',
+             markerfmt='C0o', basefmt=" ", label='Pendulum 1')
+    plt.stem(freqs, np.abs(theta2_fft), linefmt='C1-',
+             markerfmt='C1o', basefmt=" ", label='Pendulum 2')
+    plt.xlabel('Frequency (Hz)')
+    plt.ylabel('Amplitude')
+    plt.title('Frequency Analysis')
+    plt.xlim(-0.5, 0.5)  # Zooming on the x-axis, adjust the range as needed
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+
+def showJacobien():
+    # Definition of the variables for the Jacobian matrix
+    theta1_sym, dtheta1_sym, theta2_sym, dtheta2_sym = sp.symbols(
+        'theta1 dtheta1 theta2 dtheta2')
+    y_sym = sp.Matrix([theta1_sym, dtheta1_sym, theta2_sym, dtheta2_sym])
+
+    # Definition of the differential equations
+    d_theta1_sym = dtheta1_sym
+    dd_theta1_sym = A*sp.sin(theta1_sym) - B*sp.sin(theta1_sym - theta2_sym) * \
+        (1 - (d / sp.sqrt(d**2 + 2*l**2*(1 - sp.cos(theta1_sym + theta2_sym))))) - D*dtheta1_sym
+    d_theta2_sym = dtheta2_sym
+    dd_theta2_sym = A*sp.sin(theta2_sym) - C*sp.sin(theta2_sym - theta1_sym) * \
+        (1 - (d / sp.sqrt(d**2 + 2*l**2*(1 - sp.cos(theta1_sym + theta2_sym))))) - D*dtheta2_sym
+
+    # Definition of the derivative of the state vector
+    f_sym = sp.Matrix([d_theta1_sym, dd_theta1_sym,
+                      d_theta2_sym, dd_theta2_sym])
+
+    # Calcul of the Jacobian matrix
+    J = f_sym.jacobian(y_sym)
+
+    J_evaluated = J.evalf(subs={theta1_sym: theta1_stationary, dtheta1_sym: dtheta1_stationary,
+                          theta2_sym: theta2_stationary, dtheta2_sym: dtheta2_stationary})
+
+    print("\nMatrice jacobienne évaluée aux conditions initiales:")
+    sp.pprint(J_evaluated)
+    # Convert the SymPy matrix to a NumPy array
+    J_array = np.array(J_evaluated).astype(np.float64)
+
+    # Calculate the eigenvalues of the Jacobian matrix
+    eigenvalues, _ = np.linalg.eig(J_array)
+
+    # Display the eigenvalues
+    print("Eigenvalues of the Jacobian matrix at equilibrium:", eigenvalues)
+
+
+def StationarySolution(t):
+    # Guess initial values for the stationary points
+    # You can provide initial guesses based on your system
+    y_guess = [0, 0, 0, 0]
+
+    # Solve for the stationary points
+    stationary_solution = fsolve(G_adim, y_guess, t)
+
+    # Extract the stationary points
+    theta1_stationary, dtheta1_stationary, theta2_stationary, dtheta2_stationary = stationary_solution
+
+    # Print the stationary points
+    print("\nPoints stationnaires:")
+    print(f"theta1: {theta1_stationary}")
+    print(f"dtheta1: {dtheta1_stationary}")
+    print(f"theta2: {theta2_stationary}")
+    print(f"dtheta2: {dtheta2_stationary}")
+
+    return stationary_solution
+
+
+# Time points to solve the ODE for
+t = np.linspace(0, 50, 1000)
+
+# initial values for the stationary points
+theta1_stationary, dtheta1_stationary, theta2_stationary, dtheta2_stationary = StationarySolution(
+    t)
+
+# Show the Jacobian matrix
+showJacobien()
 
 # I start the class with some random coordinates
 pendulum = ball((-d/2 + width/2, l + height/4),
                 (d/2 + width/2, l + height/4), 5)
-
-# Time points to solve the ODE for
-t = np.linspace(0, 100, 1000)
 
 # Solve the ODE
 y0 = [theta1_0, dot_theta1_0, theta2_0, dot_theta1_0]
@@ -154,58 +264,28 @@ sold_theta1 = sol[:, 1]
 soltheta2 = sol[:, 2]
 sold_theta2 = sol[:, 3]
 
-# Initialize plot
-plt.ion()
-fig, ax = plt.subplots()
-line1, = ax.plot([], [], label='theta1')
-line2, = ax.plot([], [], label='theta2')
-ax.set_xlabel('Temps')
-ax.set_ylabel('Theta')
-ax.set_title('Oscillations du système en fonction du temps')
-ax.set_xlim(0, 100)
-ax.legend()
-ax.grid(True)
-plt.plot(t, soltheta1, label='theta1', color='blue')
-plt.plot(t, soltheta2, label='theta2', color='red')
-
-
-# Collect data
-theta1_data = soltheta1  # Oscillation data for pendulum 1
-theta2_data = soltheta2  # Oscillation data for pendulum 2
-
-# Sampling frequency
-fs = 1 / (t[1] - t[0])  # Sample rate, inverse of time step
-
-# Perform FFT
-theta1_fft = fft(theta1_data)
-theta2_fft = fft(theta2_data)
-
-# Frequency axis
-freqs = fftfreq(len(theta1_data), 1 / fs)
-
-# Plot FFT results
-plt.figure()
-plt.plot(freqs, np.abs(theta1_fft), label='Pendulum 1')
-plt.plot(freqs, np.abs(theta2_fft), label='Pendulum 2')
-plt.xlabel('Frequency (Hz)')
-plt.ylabel('Amplitude')
-plt.title('Frequency Analysis')
-plt.xlim(-1,1)  # Zooming on the x-axis, adjust the range as needed
-plt.legend()
-plt.grid(True)
-plt.show()
+# Plot the results
+PlotTheta(soltheta1, soltheta2)
+Plotgraph(soltheta1, sold_theta1, soltheta2, sold_theta2)
+PlotFourier(soltheta1, soltheta2)
 
 # Update plot in a loop
 t = 0
 
+# Initialisation of Pygame
+pygame.init()
+
+# Creation of the fenêtre
+background = pygame.display.set_mode((800, 800))
+
 while not Out:
     for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                Out = True  # Si la fenêtre est fermée, sortir de la boucle
-            if event.type == pygame.MOUSEBUTTONDOWN:  # Read if you want go out
-                pendulum = ball((width/2 - (d/2), height/4 + l),
-                                (width/2 + (d/2), height/4 + l), 5)
-                acceleration = True
+        if event.type == pygame.QUIT:
+            Out = True  # Si la fenêtre est fermée, sortir de la boucle
+        if event.type == pygame.MOUSEBUTTONDOWN:  # Read if you want go out
+            pendulum = ball((width/2 - (d/2), height/4 + l),
+                            (width/2 + (d/2), height/4 + l), 5)
+            acceleration = True
 
     if acceleration:   # Increase acceleration and damping in the pendulum moviment
         # Increment time
@@ -216,7 +296,7 @@ while not Out:
         sol, info = odeint(G_adim, y0, [0, t], full_output=True)
 
         # Accédez aux informations détaillées
-        #print(info)
+        # print(info)
 
         # Extract theta1 and theta2
         theta1 = sol[-1, 0]
@@ -224,29 +304,8 @@ while not Out:
 
         get_path(theta1 + pi, theta2 + pi, l)
 
-        # Update plot data
-        line1.set_xdata(np.append(line1.get_xdata(), t))
-        line1.set_ydata(np.append(line1.get_ydata(), theta1))
-        line2.set_xdata(np.append(line2.get_xdata(), t))
-        line2.set_ydata(np.append(line2.get_ydata(), theta2))
-
-        # Redraw the plot
-        ax.relim()
-        #ax.autoscale_view()
-
     redraw()
-
     # Pause to give time to redraw
     plt.pause(0.01)
-
-    # Guess initial values for the stationary points
-    # You can provide initial guesses based on your system
-    y_guess = [0, 0, 0, 0]
-
-    # Solve for the stationary points
-    stationary_solution = fsolve(G_adim, y_guess, t)
-
-    # Extract the stationary points
-    theta1_stationary, dtheta1_stationary, theta2_stationary, dtheta2_stationary = stationary_solution
 
 pygame.quit()
